160b2bca6f2c78025face7856ece8c7aaf06f516
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-08-28 16:32:55 +0100

    fixed the navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 
-import Layout from './components/Layout/Layout';
+//import Layout from './components/Layout/Layout';
+//import { BrowserRouter as Router } from 'react-router-dom';
+import NavigationBar from './components/Navigation/NavigationBar/NavigationBar';
 
 class App extends Component {
   render() {
     return (
-      <div>
-       <Layout>
-         ... 
-       </Layout>
-      </div>
+      
+        <div>
+          <NavigationBar/>
+           
+        </div>
+      
     );
   }
 }
---
+++ src/components/Navigation/NavigationBar/NavigationBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav, NavItem} from 'react-bootstrap';
+
+const navigationBar = () => (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="/">Build-a-Burger</a>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                    CONTACT
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    CHECKOUT
+                </NavItem>
+            </Nav>
+        </Navbar.Collapse> 
+    </Navbar>
+);
+
+export default navigationBar;
