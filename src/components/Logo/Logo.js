f07c442ee656cf0f842ef8295d63d6ca71ff8ee1
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-08-31 15:13:15 +0100

    added ingredients dynamically

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.css';
 
 const logo = (props) => (
     <div className={classes.Logo} style={{height: props.height}}>
-        <img src={burgerLogo}></img>
+        <img src={burgerLogo} alt="burger"/>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
+
+
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 2,
+            bacon: 5,
+            meat: 1
+        },
+        //totalPrice: 4,
+        //purchasable: false,
+        //purchasing: false
+    }
+
     render () {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
