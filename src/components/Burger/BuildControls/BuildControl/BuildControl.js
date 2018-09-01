ee4cfb846cdf58079f4d012994790c116d527bf1
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-09-01 15:24:10 +0100

    enabled the Less build contol button to removed ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,7 @@ const buildControl = (props) => (
     <div className="BuildControl">
         <div className="Label" >{props.label}</div>
         <button className="More" onClick={props.added} >More</button>
-        <button className="Less">Less</button>
+        <button className="Less" onClick={props.removed}>Less</button>
     </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,12 +37,31 @@ class BurgerBuilder extends Component {
         
     };
 
+    removeIngredientHander = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return;
+        }
+        const updatedCount = oldCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    
+    };
+
+
     render () {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler}/>
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHander}/>
             </Aux>
         );
     }
